a4d06259f5798cc4413e3aac7b2dc642623457a4
Andres Benavides <dandavi2310@hotmail.com>
2024-05-20 10:11:32 -0500

    agregando modal en nuevo componente

---
+++ src/app/components/cart-modal/cart-modal.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CartComponent } from '../cart/cart.component';
+import { CartItem } from '../../models/cartitem';
+
+@Component({
+  selector: 'cart-modal',
+  standalone: true,
+  imports: [CartComponent],
+  templateUrl: './cart-modal.component.html',
+})
+export class CartModalComponent {
+
+  @Input() items: CartItem[] = [];
+  @Input() total: number = 0;
+  @Output() idProductEventEmitter = new EventEmitter();
+  @Output() closeEventEmitter = new EventEmitter();
+
+  onDeleteCart (id: number){
+    this.idProductEventEmitter.emit(id);
+  }
+
+  closeCart(): void{
+    this.closeEventEmitter.emit();
+  }
+}
