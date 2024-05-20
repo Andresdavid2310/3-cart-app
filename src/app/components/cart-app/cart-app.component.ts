ddce7221596d85b05d5d0dca87e6e32af3e15c12
Andres Benavides <dandavi2310@hotmail.com>
2024-05-20 05:53:43 -0500

    agregando carro de compras

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -5,26 +5,67 @@ import { Product } from '../../models/product';
 import { CatalogComponent } from '../catalog/catalog.component';
 import { CartComponent } from '../cart/cart.component';
 import { CartItem } from '../../models/cartitem';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'cart-app',
   standalone: true,
-  imports: [CatalogComponent, CartComponent],
+  imports: [CatalogComponent, CartComponent, NavbarComponent],
   templateUrl: './cart-app.component.html',
 })
 export class CartAppComponent implements OnInit {
   
   products: Product[] = [];
+  
   items: CartItem[] = [];
+  
+  total: number = 0;
+
+  showCart: boolean = false;
   
   constructor(private service: ProductService){
     
   }
   ngOnInit(): void {
     this.products = this.service.findAll();
+    this.items = JSON.parse(sessionStorage.getItem('cart') || '[]') ;
+    this.calculateTotal();
   }
 
-  onAddCart(product: Product){
-    this.items = [...this.items, {product: {...product}, quantity:1 }];
+  onAddCart(product: Product): void{
+    const hasItem = this.items.find(item => item.product.id === product.id);
+    if(hasItem){
+      this.items = this.items.map(item => {
+        if(item.product.id === product.id){
+          return {
+            ...item, quantity: item.quantity + 1
+          }
+        }
+        return item;
+      });
+    }else {
+      this.items = [...this.items, {product: {...product}, quantity:1 }];
+    }
+    this.calculateTotal();
+    this.saveSession();
+  }
+
+  onDeleteCart(id: number): void{
+    this.items = this.items.filter(item =>item.product.id !== id);
+    this.calculateTotal();
+    this.saveSession();
+  }
+
+  calculateTotal(): void {
+    this.total = this.items.reduce((accumulator, item) => accumulator + item.quantity * item.product.price, 0);
+    this.saveSession();
+  }
+
+  saveSession(): void {
+    sessionStorage.setItem('cart', JSON.stringify(this.items));
+  }
+
+  openCart(): void {
+    this.showCart = !this.showCart;
   }
 }
--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CartItem } from '../../models/cartitem';
 
 @Component({
@@ -10,5 +10,13 @@ import { CartItem } from '../../models/cartitem';
 export class CartComponent {
   
   @Input() items: CartItem[] = [];
+
+  @Input() total: number = 0;
+
+  @Output() idProductEventEmitter = new EventEmitter();
+
+  onDeleteCart (id: number){
+    this.idProductEventEmitter.emit(id);
+  }
 
 }
