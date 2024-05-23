0a67f38c3f50c940dc32de47174d4e30368ddd84
Andres Benavides <dandavi2310@hotmail.com>
2024-05-23 10:38:49 -0500

    create routes for catalog in cart

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../../services/product.service';
-import { products } from '../../data/product.data';
-import { Product } from '../../models/product';
 import { CatalogComponent } from '../catalog/catalog.component';
 import { CartItem } from '../../models/cartitem';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { SharingDataService } from '../../services/sharing-data.service';
 
 @Component({
@@ -15,50 +12,62 @@ import { SharingDataService } from '../../services/sharing-data.service';
   templateUrl: './cart-app.component.html',
 })
 export class CartAppComponent implements OnInit {
-  
-  products: Product[] = [];
-  
+
   items: CartItem[] = [];
 
   total: number = 0;
-    
-  constructor(private service: ProductService, private sharingDataService :SharingDataService){}
+
+  constructor(
+    private router: Router,
+    private sharingDataService: SharingDataService) { }
 
   ngOnInit(): void {
-    this.products = this.service.findAll();
-    this.items = JSON.parse(sessionStorage.getItem('cart') || '[]') ;
+    this.items = JSON.parse(sessionStorage.getItem('cart') || '[]');
     this.calculateTotal();
     this.onDeleteCart();
+    this.onAddCart();
   }
 
-  onAddCart(product: Product): void{
-    const hasItem = this.items.find(item => item.product.id === product.id);
-    if(hasItem){
-      this.items = this.items.map(item => {
-        if(item.product.id === product.id){
-          return {
-            ...item, quantity: item.quantity + 1
+  onAddCart(): void {
+    this.sharingDataService.productEventEmitter.subscribe(product => {
+
+      const hasItem = this.items.find(item => item.product.id === product.id);
+      if (hasItem) {
+        this.items = this.items.map(item => {
+          if (item.product.id === product.id) {
+            return {
+              ...item, quantity: item.quantity + 1
+            }
           }
-        }
-        return item;
-      });
-    }else {
-      this.items = [...this.items, {product: {...product}, quantity:1 }];
-    }
+          return item;
+        });
+      } else {
+        this.items = [...this.items, { product: { ...product }, quantity: 1 }];
+      }
 
-    this.calculateTotal();
-    this.saveSession();
+      this.calculateTotal();
+      this.saveSession();
+      this.router.navigate(['/cart'], {
+        state: { items: this.items, total: this.total }
+      });
+    });
   }
 
-  onDeleteCart(): void{
-    this.sharingDataService.idProductEventEmitter.subscribe(id =>{      
-      this.items = this.items.filter(item =>item.product.id !== id);
-      if(this.items.length == 0){
+  onDeleteCart(): void {
+    this.sharingDataService.idProductEventEmitter.subscribe(id => {
+      this.items = this.items.filter(item => item.product.id !== id);
+      if (this.items.length == 0) {
         sessionStorage.removeItem('cart');
         sessionStorage.clear();
       }
       this.calculateTotal();
       this.saveSession();
+      
+      this.router.navigateByUrl('/', { skipLocationChange: true}).then(()=>{
+        this.router.navigate(['/cart'], {
+          state: { items: this.items, total: this.total }
+        });
+      })
     })
   }
 
